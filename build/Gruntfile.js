7a9408a5d101e55b433277f083b9f85447a2239c
Daku <admin@codeanimu.net>
2015-09-19 09:40:57 +0100

    jade careers

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -108,7 +108,8 @@ module.exports = function(grunt){
 					// }
 				},
 				files: {
-					"./files/templates/partials/education.html": "./files/templates/partials/education.jade"
+					"./files/templates/partials/education.html" : "./files/templates/partials/education.jade",
+					"./files/templates/partials/career.html"    : "./files/templates/partials/career.jade",
 				}
 			}
 		},
